3b88424c4d751084d345d0a09136fa145b514b79
Oleg Krasnov <oleg@vadagama.ru>
2019-10-07 00:18:12 +0300

    edit cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,10 @@
-const routerCards = require('express').Router();
+const cardsRouter = require('express').Router();
+const cards = require('../data/cards');
 
 const getCards = (req, res) => {
-  res.send(req.cards);
+  res.send(cards);
 };
 
-routerCards.get('/cards', getCards);
+cardsRouter.get('/cards', getCards);
 
-module.exports = routerCards;
+module.exports = cardsRouter;
